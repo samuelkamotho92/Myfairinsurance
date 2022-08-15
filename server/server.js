64edd3e88dde156494eda8d5ae335af3ec19cc6d
samuelkamotho92 <samuelkamotho92@gmail.com>
2022-08-15 19:17:27 +0300

    connected to heroku

--- server/server.js
+++ server/server.js
@@ -1,9 +1,25 @@
 const mongoose =  require("mongoose");
+const express = require("express");
+const path = require('path');
 const dotenv = require("dotenv");
 dotenv.config({path:'./config.env'});
 const app = require("./app.js");
 const port = process.env.PORT || 8080;
 const dBurl = process.env.DATABASE_URL;
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join("public")));
+    const myPath = '/Users/user/Documents/Myfairinsurance';
+    app.get("*", function (_, res) {
+        console.log(__dirname);
+        res.sendFile(path.join(__dirname + "/public/index.html"),
+          function (err) {
+            if (err) {
+              res.status(500).send(err);
+            }
+          }
+        );
+      });
+    }
 mongoose.connect(dBurl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
